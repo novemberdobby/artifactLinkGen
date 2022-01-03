78c05bf9a7e76b208af8d55fbfda77b10bbded10
Jon <novemberdobby@gmail.com>
2022-01-03 15:07:25 +0000

    load in screens

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
-﻿using HadesBoonBot;
-using System.Text.Json;
+﻿using System.Text.Json;
+using Cv2 = OpenCvSharp.Cv2;
+using OCV = OpenCvSharp;
 
 namespace HadesBoonBot
 {
@@ -11,6 +12,21 @@ namespace HadesBoonBot
             using (StreamReader data = new(args[0]))
             {
                 inputData = JsonSerializer.Deserialize<TrainingData>(data.ReadToEnd())!;
+            }
+
+            foreach (TrainingData.Screen screen in inputData.Screens)
+            {
+                if(!File.Exists(screen.FileName))
+                {
+                    Console.WriteLine($"Skipping {screen.FileName} due to missing file");
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine($"Reading {screen.FileName}");
+                }
+
+                OCV.Mat image = Cv2.ImRead(screen.FileName);
             }
 
             return 0;
