4f52f45d47474fc401b2d09e79a0fe3351fe3aa6
Jon <novemberdobby@gmail.com>
2022-12-16 17:59:16 +0200

    first pass on displaying trait names & descriptions on hover include pin tray items in hover check

--- Processors/WebPage.cs
+++ Processors/WebPage.cs
@@ -1,4 +1,5 @@
-﻿using OCV = OpenCvSharp;
+﻿using System.Text;
+using OCV = OpenCvSharp;
 
 namespace HadesBoonBot.Processors
 {
@@ -24,8 +25,11 @@ namespace HadesBoonBot.Processors
                     file.WriteLine($"<html>");
                     file.WriteLine($"<body>");
 
-                    //display the screen
+                    //display the screen itself
                     file.WriteLine($"<img class='zeroed' src='{screen.RemoteSource}' />");
+
+                    StringBuilder traitData = new();
+                    traitData.AppendLine("trait_data = {}");
 
                     //add a div per tray trait
                     foreach (var trayItem in screen.Screen.Slots)
@@ -38,20 +42,48 @@ namespace HadesBoonBot.Processors
 
                         if (meta.TryGetTraitRect(trayItem.Col, trayItem.Row, out var iconRect) && iconRect.HasValue)
                         {
+                            string id = $"trait_tray_{trayItem.Col}_{trayItem.Row}";
                             string posStyle = $"width: {iconRect.Value.Width}px; height: {iconRect.Value.Height}px; left: {iconRect.Value.Left}px; top: {iconRect.Value.Top}px";
-                            file.WriteLine($"<div class='trait' style='{posStyle};'></div>");
+                            file.WriteLine($"<div class='trait' id='{id}' style='{posStyle};'></div>");
+
+                            var info = GetSafeTraitInfo(trayItem.Trait);
+                            traitData.AppendLine($"trait_data['{id}'] = {info};");
                         }
                     }
 
-                    //find a sensible position for the info box
+                    //add a div per pin row trait
                     int columnCount = screen.Screen.GetColumnCount();
-                    int infoBoxLeft = meta.GetPinRect(columnCount, 0).iconRect.Left;
+                    foreach (var pinItem in screen.Screen.PinSlots)
+                    {
+                        string id = $"trait_pins_{pinItem.Row}";
+                        var pinIconRect = meta.GetPinRect(columnCount, pinItem.Row).iconRect;
+
+                        //deflate slightly; tray icons have a frame but pin rows don't
+                        pinIconRect.Inflate(-(int)(pinIconRect.Width * 0.1f), -(int)(pinIconRect.Height * 0.1f));
+
+                        string posStyle = $"width: {pinIconRect.Width}px; height: {pinIconRect.Height}px; left: {pinIconRect.Left}px; top: {pinIconRect.Top}px";
+                        file.WriteLine($"<div class='trait' id='{id}' style='{posStyle};'></div>");
+
+                        var info = GetSafeTraitInfo(pinItem.Trait);
+                        traitData.AppendLine($"trait_data['{id}'] = {info};");
+                    }
+
+                    //find a sensible position for the info box
+                    var pinRect = meta.GetPinRect(columnCount, 0).iconRect;
+                    int infoBoxLeft = pinRect.Right + pinRect.Width / 2;
                     int infoBoxTop = image.Height / 3;
                     int infoBoxWidth = image.Width / 2;
                     int infoBoxHeight = image.Height / 3;
 
-                    file.WriteLine($"<svg id='infoBoxLink' class='zeroed' style='display: none;' width='{image.Width}' height='{image.Height}'><line x1='0' y1='0' x2='0' y2='0' stroke='white' stroke-width='3' stroke-linecap='round'/></svg>");
-                    file.WriteLine($"<div id='infoBox' style='display: none; position: absolute; left: {infoBoxLeft}px; top: {infoBoxTop}px; width: {infoBoxWidth}px; height: {infoBoxHeight}px; border: 3px solid white; background: rgba(1, 1, 1, 0.9); color: white; overflow: hidden; line-break: auto;'></div>");
+                    file.WriteLine($@"
+<svg id='infoBoxLink' class='zeroed' style='display: none;' width='{image.Width}' height='{image.Height}'><line x1='0' y1='0' x2='0' y2='0' stroke='white' stroke-width='3' stroke-linecap='round'/></svg>
+<div id='infoBox' style='display: none;'>
+  <div id='infoBoxTraitName'></div>
+  <div id='infoBoxTraitDesc'></div>
+</div>
+");
+                    file.WriteLine($"");
+                    file.WriteLine($"");
                     
                     //styles & script
                     file.WriteLine(@"
@@ -59,27 +91,45 @@ namespace HadesBoonBot.Processors
 .trait {
   position: absolute;
   transform: rotateZ(45deg) scale(0.9);
-  animation: traitHover 2s infinite;
-}
-
-@keyframes traitHover {
-  0% {
-    opacity: 40%;
-  }
-  
-  50% {
-    opacity: 100%;
-  }
-  
-  100% {
-    opacity: 40%;
-  }
 }
 
 .zeroed {
   position: absolute;
-  left: 0px; top: 0px;
+  left: 0px;
+  top: 0px;
 }
+
+#infoBox {
+"
++
+@$"
+left: {infoBoxLeft}px;
+top: {infoBoxTop}px;
+width: {infoBoxWidth}px;
+height: {infoBoxHeight}px;
+"
++
+@"
+  position: absolute;
+  border: 3px double white;
+  background: rgba(1, 1, 1, 0.9);
+  color: white;
+  overflow: hidden;
+  line-break: auto;
+  box-sizing: border-box;
+  padding: 1em;
+}
+
+#infoBoxTraitName {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: x-large;
+  padding-bottom: 0.5em;
+}
+
+#infoBoxTraitDesc {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
 </style>
 
 <script type=""text/javascript"">
@@ -113,6 +163,8 @@ function CheckTraits(event) {
 
 function SetActiveTrait(trait) {
   var allTraits = document.getElementsByClassName('trait');
+
+  //hide all
   for (var i = 0; i < allTraits.length; i++) {
     allTraits[i].style.border = '';
   }
@@ -125,7 +177,16 @@ function SetActiveTrait(trait) {
   
   infoBox.style.display = '';
   var infoBoxBox = infoBox.getBoundingClientRect();
-  
+
+  //data for each trait
+"
++
+traitData.ToString()
++
+@"
+  document.getElementById('infoBoxTraitName').innerText = trait_data[trait.id].name;
+  document.getElementById('infoBoxTraitDesc').innerText = trait_data[trait.id].desc;
+
   infoBoxLink.style.display = '';
   var line = infoBoxLink.children[0];
   line.x1.baseVal.value = traitBox.right - 2;
@@ -143,6 +204,16 @@ document.addEventListener('mousemove', (event) => CheckTraits(event));
                     file.WriteLine($"</html>");
                 }
             }
+        }
+
+        private string GetSafeTraitInfo(Codex.Provider.Trait trait)
+        {
+            string makeSafe(string input) => input.Replace("'", "\\'").Replace("\n", "\\n");
+
+            string safeName = makeSafe(trait.Name);
+            string safeDesc = makeSafe(trait.Description);
+
+            return $"{{ name: '{safeName}', desc: '{safeDesc}' }}";
         }
     }
 }
