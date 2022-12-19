8c6c7d135cf4ad21f0545fa4e2d601c8aa0f7a98
Jon <novemberdobby@gmail.com>
2022-12-19 13:59:10 +0200

    visual tweaks to webpages added icons support HTML in trait descriptions

--- Processors/WebPage.cs
+++ Processors/WebPage.cs
@@ -46,7 +46,7 @@ namespace HadesBoonBot.Processors
                             string posStyle = $"width: {iconRect.Value.Width}px; height: {iconRect.Value.Height}px; left: {iconRect.Value.Left}px; top: {iconRect.Value.Top}px";
                             file.WriteLine($"<div class='trait' id='{id}' style='{posStyle};'></div>");
 
-                            var info = GetSafeTraitInfo(trayItem.Trait);
+                            var info = PrepareTraitInfo(trayItem.Trait, codex, OutputPage);
                             traitData.AppendLine($"trait_data['{id}'] = {info};");
                         }
                     }
@@ -61,27 +61,40 @@ namespace HadesBoonBot.Processors
                         //deflate slightly; tray icons have a frame but pin rows don't
                         pinIconRect.Inflate(-(int)(pinIconRect.Width * 0.1f), -(int)(pinIconRect.Height * 0.1f));
 
-                        string posStyle = $"width: {pinIconRect.Width}px; height: {pinIconRect.Height}px; left: {pinIconRect.Left}px; top: {pinIconRect.Top}px";
-                        file.WriteLine($"<div class='trait' id='{id}' style='{posStyle};'></div>");
+                        string posStyle = $"left: {pinIconRect.Left}px; top: {pinIconRect.Top}px; width: {pinIconRect.Width}px; height: {pinIconRect.Height}px;";
+                        file.WriteLine($"<div class='trait' id='{id}' style='{posStyle}'></div>");
 
-                        var info = GetSafeTraitInfo(pinItem.Trait);
+                        var info = PrepareTraitInfo(pinItem.Trait, codex, OutputPage);
                         traitData.AppendLine($"trait_data['{id}'] = {info};");
                     }
 
                     //find a sensible position for the info box
                     var pinRect = meta.GetPinRect(columnCount, 0).iconRect;
-                    int infoBoxLeft = pinRect.Right + pinRect.Width / 2;
-                    int infoBoxTop = image.Height / 3;
-                    int infoBoxWidth = image.Width / 2;
-                    int infoBoxHeight = image.Height / 3;
+                    OCV.Rect infoBoxRect = new(pinRect.Right + pinRect.Width / 2, image.Height / 3, image.Width / 2, image.Height / 3);
+
+                    //offset a little for the border, scale up
+                    pinRect.Left = infoBoxRect.Left + 5;
+                    pinRect.Top = infoBoxRect.Top + 5;
+                    pinRect.Width = (int)(pinRect.Width * 1.5f);
+                    pinRect.Height = (int)(pinRect.Height * 1.5f);
+                    infoBoxRect.Height = pinRect.Height + 10;
 
                     file.WriteLine($@"
-<svg id='infoBoxLink' class='zeroed' style='display: none;' width='{image.Width}' height='{image.Height}'><line x1='0' y1='0' x2='0' y2='0' stroke='white' stroke-width='3' stroke-linecap='round'/></svg>
+<svg id='infoBoxLink' class='zeroed' style='display: none;' width='{image.Width}' height='{image.Height}'><path id='curve' d='' stroke='white' stroke-width='3' stroke-linecap='round' fill='transparent'/>
+  <circle cx='' cy='' r='4' fill='#ffffff'>
+    <animateMotion dur='1s' repeatCount='indefinite'>
+      <mpath xlink:href='#curve'></mpath>
+    </animateMotion>
+  </circle>
+</svg>
 <div id='infoBox' style='display: none;'>
   <div id='infoBoxTraitName'></div>
   <div id='infoBoxTraitDesc'></div>
 </div>
 ");
+                    //and the icon
+                    file.WriteLine($@"<img id='infoBoxTraitIcon' style='display: none; position: absolute; left: {pinRect.Left}px; top: {pinRect.Top}px; width: {pinRect.Width}px; height: {pinRect.Height}px;'></img>");
+                    
                     file.WriteLine($"");
                     file.WriteLine($"");
                     
@@ -103,21 +116,23 @@ namespace HadesBoonBot.Processors
 "
 +
 @$"
-left: {infoBoxLeft}px;
-top: {infoBoxTop}px;
-width: {infoBoxWidth}px;
-height: {infoBoxHeight}px;
+left: {infoBoxRect.Left}px;
+top: {infoBoxRect.Top}px;
+width: {infoBoxRect.Width}px;
+height: {infoBoxRect.Height}px;
+padding: 1em;
+padding-left: {pinRect.Right - infoBoxRect.Left}px;
 "
 +
 @"
   position: absolute;
   border: 3px double white;
+  box-shadow: rgb(0, 0, 0) 0px 0px 40px 20px;
   background: rgba(1, 1, 1, 0.9);
   color: white;
   overflow: hidden;
   line-break: auto;
   box-sizing: border-box;
-  padding: 1em;
 }
 
 #infoBoxTraitName {
@@ -128,6 +143,7 @@ height: {infoBoxHeight}px;
 
 #infoBoxTraitDesc {
   font-family: Arial, Helvetica, sans-serif;
+  font-size: large;
 }
 
 </style>
@@ -158,10 +174,16 @@ function CheckTraits(event) {
   if (!anyActive) {
     document.getElementById('infoBox').style.display = 'none';
     document.getElementById('infoBoxLink').style.display = 'none';
+    document.getElementById('infoBoxTraitIcon').style.display = 'none';
   }
 }
 
 function SetActiveTrait(trait) {
+
+  if (trait.style.border != '') {
+    return;
+  }
+
   var allTraits = document.getElementsByClassName('trait');
 
   //hide all
@@ -184,15 +206,30 @@ function SetActiveTrait(trait) {
 traitData.ToString()
 +
 @"
-  document.getElementById('infoBoxTraitName').innerText = trait_data[trait.id].name;
-  document.getElementById('infoBoxTraitDesc').innerText = trait_data[trait.id].desc;
+  //fill text fields
+  var data = trait_data[trait.id];
+  document.getElementById('infoBoxTraitName').innerText = data.name;
+  document.getElementById('infoBoxTraitDesc').innerHTML = data.desc;
 
+  //position info box
   infoBoxLink.style.display = '';
   var line = infoBoxLink.children[0];
-  line.x1.baseVal.value = traitBox.right - 2;
-  line.y1.baseVal.value = traitBox.top + traitBox.height / 2;
-  line.x2.baseVal.value = infoBoxBox.left;
-  line.y2.baseVal.value = infoBoxBox.top + infoBoxBox.height / 2;
+
+  //draw curve
+  var c1x = (traitBox.right - 2) + (infoBoxBox.left - traitBox.right) / 2;
+  var c1y = traitBox.top + traitBox.height / 2;
+  var c2x = infoBoxBox.left - (infoBoxBox.left - traitBox.right) / 2;
+  var c2y = infoBoxBox.top + infoBoxBox.height / 2;
+
+  var d = 'M' + (traitBox.right - 2) + ',' + (traitBox.top + traitBox.height / 2) + ' C' +
+    c1x + ',' + c1y + ' ' + c2x + ',' + c2y + ' ' + infoBoxBox.left + ',' + (infoBoxBox.top + infoBoxBox.height / 2);
+
+  line.setAttributeNS(null, 'd', d);
+
+  //set icon
+  var infoBoxIcon = document.getElementById('infoBoxTraitIcon');
+  infoBoxIcon.style.display = '';
+  infoBoxIcon.src = data.icon;
 }
 
 document.addEventListener('mousemove', (event) => CheckTraits(event));
@@ -206,14 +243,31 @@ document.addEventListener('mousemove', (event) => CheckTraits(event));
             }
         }
 
-        private string GetSafeTraitInfo(Codex.Provider.Trait trait)
+        private static string PrepareTraitInfo(Codex.Provider.Trait trait, Codex codex, string outputPage)
         {
             string makeSafe(string input) => input.Replace("'", "\\'").Replace("\n", "\\n");
 
             string safeName = makeSafe(trait.Name);
             string safeDesc = makeSafe(trait.Description);
 
-            return $"{{ name: '{safeName}', desc: '{safeDesc}' }}";
+            string outputFolder = Path.Combine(Path.GetDirectoryName(outputPage)!, "icons");
+            string iconPath = Path.Combine(outputFolder, trait.Name + ".png");
+
+            if(!File.Exists(iconPath))
+            {
+                if (!Directory.Exists(outputFolder))
+                {
+                    Directory.CreateDirectory(outputFolder);
+                }
+
+                trait.Icon!.SaveImage(iconPath);
+            }
+
+            DirectoryInfo pageInfo = new(outputPage);
+            DirectoryInfo iconInfo = new(iconPath);
+
+            string relativePath = iconInfo.FullName[pageInfo.Parent!.FullName.Length..].Replace('\\', '/');
+            return $"{{ name: '{safeName}', desc: '{safeDesc}', icon: '.{makeSafe(relativePath)}' }}";
         }
     }
 }
